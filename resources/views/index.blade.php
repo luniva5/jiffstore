0612da07451c8a7b90f23f1dd325c282fa221306
Luniva Shrestha <86938743+luniva5@users.noreply.github.com>
2021-08-03 21:10:04 +0545

    Update index.blade.php

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,9 +2,9 @@
 @section("content")
 <section id="home">
     <div class="container-main">
-         <h1>Lets thrift</h1>
+         <h1>Let's thrift</h1>
          <p>COME THRIFT WITH US</p>
-         <button>Shop Now</button>
+         <button class="shop-btn">Shop Now</button>
      </div>    
  </section>
  <!--FOR PRODUCTS -->
@@ -17,52 +17,64 @@
 
     <div class="row">
         <div class="product">
-            <img class="img-fluid" src="images\2.jpg" alt="Book">
+            <img class="img-fluid" src="/images/2.jpg" alt="Book">
             <h5 class="product-name">Book</h5>
             <h4 class="product-price">Rs 250</h4>
-            <button class="buy-btn">Buy now</button>
+            <button>Buy now</button>
          </div>
         <div class="product">
-            <img class="img-fluid" src="images\3.jpg" alt="Trousers">
-            <h5 >Trousers</h5>
-            <h4>Rs 850</h4>
-            <button class="buy-btn">Buy now</button>
+            <img class="img-fluid" src="/images/3.jpg" alt="Joggers">
+            <h5 class="product-name">Joggers</h5>
+            <h4 class="product-price">Rs 850</h4>
+            <button>Buy now</button>
         </div>
         <div class="product">
-            <img class="img-fluid" src="images\1.jpg" alt="Flannel">
-            <h5>Flannel</h5>
-            <h4>Rs 800</h4>
-            <button class="buy-btn">Buy now</button>
+            <img class="img-fluid" src="/images/1.jpg" alt="Flannel">
+            <h5 class="product-name">Flannel</h5>
+            <h4 class="product-price">Rs 800</h4>
+            <button>Buy now</button>
         </div>
         <div class="product">
-            <img class="img-fluid" src="images\4.jpg" alt="Hoodie">
-            <h5>Hoodie</h5>
-            <h4>Rs 1250</h4>
-            <button class="buy-btn">Buy now</button>
+            <img class="img-fluid" src="/images/4.jpg" alt="Hoodie">
+            <h5 class="product-name">Hoodie</h5>
+            <h4 class="product-price">Rs 1250</h4>
+            <button>Buy now</button>
         </div>
         <div class="product">
-            <img class="img-fluid" src="images\5.jpg" alt="Converse">
-            <h5>Converse</h5>
-            <h4>Rs 1300</h4>
-            <button class="buy-btn">Buy now</button>
+            <img class="img-fluid" src="/images/5.jpg" alt="Converse">
+            <h5 class="product-name">Converse</h5>
+            <h4 class="product-price">Rs 1300</h4>
+            <button>Buy now</button>
         </div>
         <div class="product">
-            <img class="img-fluid" src="images\6.jpg" alt="Chain">
-            <h5>Gold plated Chain</h5>
-            <h4>Rs 850</h4>
-            <button class="buy-btn">Buy now</button>
+            <img class="img-fluid" src="/images/6.jpg" alt="Chain">
+            <h5 class="product-name">Gold plated Chain</h5>
+            <h4 class="product-price">Rs 850</h4>
+            <button>Buy now</button>
         </div>
         <div class="product">
-            <img class="img-fluid" src="images\7.jpg" alt="Bomber jacket">
-            <h5>Bomber Jacket</h5>
-            <h4>Rs 1450</h4>
-            <button class="buy-btn">Buy now</button>
+            <img class="img-fluid" src="/images/7.jpg" alt="Leather Bomber Jacket">
+            <h5 class="product-name">Leather Bomber Jacket</h5>
+            <h4 class="product-price">Rs 2450</h4>
+            <button>Buy now</button>
         </div>
         <div class="product">
-            <img class="img-fluid" src="images\8.jpg" alt="Hand bag">
-            <h5>Hand bag</h5>
-            <h4>Rs 1750</h4>
-            <button class="buy-btn">Buy now</button>
+            <img class="img-fluid" src="/images/8.jpg" alt="Hand bag">
+            <h5 class="product-name">Hand bag</h5>
+            <h4 class="product-price">Rs 1750</h4>
+            <button>Buy now</button>
+        </div>
+        <div class="product">
+            <img class="img-fluid" src="/images/9.jpg" alt="Biker Shorts">
+            <h5 class="product-name">Black Biker Shorts</h5>
+            <h4 class="product-price">Rs 1250</h4>
+            <button>Buy now</button>
+        </div>
+        <div class="product">
+            <img class="img-fluid" src="/images/10.jpg" alt="Graphics Tee">
+            <h5 class="product-name">Graphics Tee</h5>
+            <h4 class="product-price">Rs 850</h4>
+            <button>Buy now</button>
         </div>
     </div>
  </section>
