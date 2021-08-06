e2ad42453e64070c5b273c22e4168ef280b7e607
Luniva Shrestha <86938743+luniva5@users.noreply.github.com>
2021-08-06 14:04:41 +0545

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,17 +15,55 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-//Route::get('/laravel', function () {
- //   return view('welcome');
-//});
+Route::get('/laravel', function () {
+   return view('welcome');
+});
 Route::get('/', function () {
     return view('index');
 });
 Route::get('/login', function () {
     return view('login');
 });
+Route::post("/login",[UserController::class,'login']);
 Route::get('/cart', function () {
     return view('cart');
+});
+//details
+Route::get('/details.book', function () {
+    return view('details.book');
+});
+Route::get('/details.joggers', function () {
+    return view('details.joggers');
+});
+Route::get('/details.flannel', function () {
+    return view('details.flannel');
+});
+Route::get('/details.hoodie', function () {
+    return view('details.hoodie');
+});
+Route::get('/details.converse', function () {
+    return view('details.converse');
+});
+Route::get('/details.chain', function () {
+    return view('details.chain');
+});
+Route::get('/details.leather', function () {
+    return view('details.leather');
+});
+Route::get('/details.bag', function () {
+    return view('details.bag');
+});
+Route::get('/details.bikershorts', function () {
+    return view('details.bikershorts');
+});
+Route::get('/details.graphictee', function () {
+    return view('details.graphictee');
+});
+Route::get('/details.croptop', function () {
+    return view('details.croptop');
+});
+Route::get('/details.cargopants', function () {
+    return view('details.cargopants');
 });
 Route::get('/checkout', function () {
     return view('checkout');
